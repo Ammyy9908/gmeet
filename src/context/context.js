69ff1515c1e89c3b771e2909aade157680f043a8
Ammyy9908 <sb78639@gmail.com>
2021-06-19 07:58:05 +0530

    production server added

--- src/context/context.js
+++ src/context/context.js
@@ -1,6 +1,9 @@
 import socketio from "socket.io-client";
 import React from "react"
+import dotenv from "dotenv";
+
+dotenv.config();
 
 
-export const socket = socketio('http://localhost:5000',{ transports: ["websocket"] });
+export const socket = socketio(process.env.REACT_APP_SERVER_PROD,{ transports: ["websocket"] });
 export const SocketContext = React.createContext();
